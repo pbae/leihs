8bcdf3a44ec41b4d9e65f7c028d0e53ea0085248
Thomas Schank <DrTom@schank.ch>
2016-12-08 20:04:38 +0100

    Add in v3 yet missing fkey constraints

---
+++ db/migrate/009_add_missing_fkeys.rb
@@ -0,0 +1,15 @@
+class AddMissingFkeys < ActiveRecord::Migration
+  def change
+    add_foreign_key :hidden_fields, :fields, on_delete: :cascade
+    add_foreign_key :hidden_fields, :users, on_delete: :cascade
+    add_foreign_key :mail_templates, :inventory_pools, on_delete: :cascade
+    add_foreign_key :mail_templates, :languages, on_delete: :cascade
+    add_foreign_key :procurement_category_inspectors, :users
+    add_foreign_key :procurement_requests, :locations
+    add_foreign_key :procurement_requests, :models
+    add_foreign_key :procurement_requests, :suppliers
+    add_foreign_key :procurement_requests, :users
+    add_foreign_key :procurement_templates, :models
+    add_foreign_key :procurement_templates, :suppliers
+  end
+end
